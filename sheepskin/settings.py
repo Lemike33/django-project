3e802699d8d01c2f3926b8bb0d620266180caaf2
Lemike33 <leshukovv87@mail.ru>
2022-02-08 16:58:48 +0300

    update requirements file

--- sheepskin/settings.py
+++ sheepskin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,6 +161,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "pictures")
 
 LOGIN_REDIRECT_URL = 'start_page'
 LOGIN_URL = 'user'
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
