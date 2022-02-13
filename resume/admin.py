c9befb8a3037858df0d231df44c047a7a608465e
Lemike33 <leshukovv87@mail.ru>
2022-02-13 21:26:07 +0300

    update view model Job in admin panel

--- resume/admin.py
+++ resume/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Job
 
-admin.site.register(Job)
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'img', 'date_start', 'date_end', 'site')
+    prepopulated_fields = {'slug': ('title',), }
+
+
+admin.site.register(Job, JobAdmin)
